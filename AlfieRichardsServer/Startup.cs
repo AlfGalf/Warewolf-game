4c163d697ab98c2f9ef4b6da099e8f3407545fc9
Alfie Richards <arichardsdev@gmail.com>
2020-09-21 17:23:13 +0100

    Changed the source path

--- AlfieRichardsServer/Startup.cs
+++ AlfieRichardsServer/Startup.cs
@@ -24,7 +24,7 @@ namespace AlfieRichardsServer
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = $"ClientApp/build"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,10 +56,9 @@ namespace AlfieRichardsServer
 
             app.UseSpa(spa =>
             {
-                spa.Options.SourcePath = "ClientApp";
-
                 if (env.IsDevelopment())
                 {
+                    spa.Options.SourcePath = "ClientApp";
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
