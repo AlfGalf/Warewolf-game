b0cc069aa712b11238499a810b542db509c1576c
Alfie Richards <arichardsdev@gmail.com>
2020-09-22 16:17:23 +0100

    Added print line

--- AlfieRichardsServer/Program.cs
+++ AlfieRichardsServer/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Logging;
 
 namespace AlfieRichardsServer
@@ -17,21 +18,30 @@ namespace AlfieRichardsServer
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => 
-                { 
-                    var p = System.Reflection.Assembly.GetEntryAssembly().Location;
-                    p = p.Substring(0, p.LastIndexOf(@"\") + 1);
-                    
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    string p = "";
+                    if (string.Equals(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), "development", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        p = System.Reflection.Assembly.GetEntryAssembly().Location;
+                        p = p.Substring(0, p.LastIndexOf(@"\") + 1);
+
+                    }
+                    else
+                    {
+                        p = "/webserver/current";
+                    }
+                        
                     Console.WriteLine(p);
 
-                    webBuilder.ConfigureKestrel(serverOptions =>
-                    {
-                        
-                    })
-                        .UseContentRoot(p)
-                        .UseStartup<Startup>(); 
+                    webBuilder.ConfigureKestrel(serverOptions => { });
+                    webBuilder.UseContentRoot(p);
+                    webBuilder.UseStartup<Startup>();
                 });
+        }
+
     }
 }
