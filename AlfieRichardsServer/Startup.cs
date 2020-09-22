fd64841f29ff6428af9341e5580a1ab198e3dc6d
Alfie Richards <arichardsdev@gmail.com>
2020-09-22 14:13:06 +0100

    Changed to be inline with an article

--- AlfieRichardsServer/Startup.cs
+++ AlfieRichardsServer/Startup.cs
@@ -26,7 +26,7 @@ namespace AlfieRichardsServer
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = $"ClientApp"; 
+                configuration.RootPath = $"ClientApp/build"; 
             });
         }
 
@@ -59,7 +59,7 @@ namespace AlfieRichardsServer
 
             app.UseSpa(spa =>
             {
-                spa.Options.SourcePath = "ClientApp/build";
+                spa.Options.SourcePath = "ClientApp";
                 
                 if (env.IsDevelopment())
                 {
